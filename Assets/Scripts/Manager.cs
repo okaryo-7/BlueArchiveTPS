329e5f234ed623864c04c840bcd9609aa0fde5e6
okaryo-7 <ev190568@meiji.ac.jp>
2024-08-06 04:31:15 +0900

    visual stdioをインストール

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementStateManager : MonoBehaviour
+public class Manager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
